05d12083e622f381174b3b1f36b2916a35c6c515
Manuel Hernandez <manuelhe46@gmail.com>
2020-03-23 14:41:16 -0700

    banish classes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Stage, Layer, Text } from 'react-konva';
 import ColoredRect from './components/coloredRect';
 import './main.scss';
 
-export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    //console.log('in App.constructor this.props is ', this.props);
-    this.state={ 
-      chaos: {}, 
-      app:{}, 
-      randig:0
-    };
-    this.refresh = this.refresh.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  } 
-  static getDerivedStateFromProps(nextProps, prevState){
-    //console.log('in planzu.getDerivedStateFromProps  nextProps is ', nextProps);
-    //console.log('in planzu.getDerivedStateFromProps  prevState is ', prevState);
-    return{chaos: nextProps.chaos};
-  }
-  handleClick(){
-    console.log('clicked');
-    this.setState({randig:Math.random()});
+const  App = (props) => {
+  const [randomDigit, setRandomDigit] = useState(0);
 
+  const handleClick = () =>{
+    setRandomDigit(Math.random());
   }
-  componentDidMount(){
-    //console.log('in App.componentWicomponentDidMountllReceiveProps nextProps is ');
-    this.props.initChaos();
-    this.props.loadChaos();
-  }
-  refresh(){
-      try {
-        console.log('in app refresh');
-        this.props.loadPlanzu();
-      } catch(error){
-        console.log('error in app error is ', error);
-      }
-  }
-  render(){
-    return (
-      <div className="App" onClick={this.handleClick} onTap={this.handleClick}>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-            <Text text="Try click on rect" />
-            <ColoredRect digit={this.state.randig}/>
-          </Layer>
-        </Stage>
-      </div>
-    );
-  }
+
+  return (
+    <div className="App" onClick={handleClick} onTap={handleClick}>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try click on rect" />
+          <ColoredRect digit={randomDigit} onClick={handleClick} onTap={handleClick}/>
+        </Layer>
+      </Stage>
+    </div>
+  );
 }
+export default App;
--- src/components/coloredRect.js
+++ src/components/coloredRect.js
@@ -2,29 +2,18 @@ import React from 'react';
 import { Rect } from 'react-konva';
 import Konva from 'konva';
 
-export default class ColoredRect extends React.Component {
-  state = {
-    color: 'green'
-  };
-  displayRandom = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <Rect
-        x={ Math.random() * (window.innerWidth - 100)}
-        y={ Math.random() * (window.innerHeight - 100)}
-        width={20 +  Math.random() * 100}
-        height={ 20 + Math.random() * 100 }
-        fill={Konva.Util.getRandomColor()}
-        shadowBlur={5}
-        //r = {Math.floor(Math.random() * 256)}
-        //g = {Math.floor(Math.random() * 256)}
-        //b = {Math.floor(Math.random() * 256)"
-
-      />
-    );
-  }
+const  ColoredRect = (props) =>  {
+  return (
+    <Rect
+      x={ Math.random() * (window.innerWidth - 200)}
+      y={ Math.random() * (window.innerHeight - 200)}
+      width={20 +  Math.random() * 200}
+      height={ 20 + Math.random() * 200 }
+      fill={Konva.Util.getRandomColor()}
+      shadowBlur={2}
+      onClick={props.handleClick} 
+      onTap={props.handleClick}
+    />
+  );
 }
+export default ColoredRect;
